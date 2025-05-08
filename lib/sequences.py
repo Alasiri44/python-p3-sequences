af247ca262cd183e6644767d31b97ea529f57af1
alasiri <austinalasiri44@gmail.com>
2025-05-08 04:48:40 +0300

    done tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+
+#function to put the fibonacci
+def print_fibonacci(length):  
+    #defining the list
+    my_list = [0, 1]
+    
+    if(length == 0):
+        my_list.clear()
+        print(my_list)
+    elif(length < 3):
+        print(my_list[0:length] )
+        
+    else:
+        for i in range(length - 2):
+            my_list.append(my_list[-1] + my_list[-2])
+        print(my_list)
+    
+    
+print_fibonacci(1)
+
+
